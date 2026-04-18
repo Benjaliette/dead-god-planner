75da7792119f4874ea62521de7358955e6992553
Benjamin Liet <benjamin.liet.dev@gmail.com>
2026-04-18 19:11:16 +0200

    initialization of dgp projet with dgp parent module

---
+++ ddg/src/test/java/com/dgp/dead_god_planner/DeadGodPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgp.dead_god_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadGodPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
