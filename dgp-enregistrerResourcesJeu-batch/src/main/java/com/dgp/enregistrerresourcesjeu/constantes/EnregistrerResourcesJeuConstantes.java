f1741b1f8f646511b3bfec7f0dea681049d1d742
Benjamin Liet <benjamin.liet.dev@gmail.com>
2026-05-10 22:16:15 +0200

    FEAT : add dockerfile to batch project + add prod url

--- dgp-enregistrerResourcesJeu-batch/src/main/java/com/dgp/enregistrerresourcesjeu/constantes/EnregistrerResourcesJeuConstantes.java
+++ dgp-enregistrerResourcesJeu-batch/src/main/java/com/dgp/enregistrerresourcesjeu/constantes/EnregistrerResourcesJeuConstantes.java
@@ -3,7 +3,7 @@ package com.dgp.enregistrerresourcesjeu.constantes;
 import java.util.List;
 
 public final class EnregistrerResourcesJeuConstantes {
-    public static final String BASE_ASSETS_URL = "http://jq8zqnl4lzomtrujfdrkadqk.88.182.27.241.sslip.io/gfx/";
+    public static final String BASE_ASSETS_URL = "https://resources.deadgodplanner.click/gfx/";
 
     public static final List<Long> NON_PLAYABLE_PLAYERS = List.of(11L, 12L, 17L, 20L, 38L, 39L, 40L);
 }
--- dgp-enregistrerResourcesJeu-batch/src/main/java/com/dgp/enregistrerresourcesjeu/controller/EnregistrerResourcesJeuBatchController.java
+++ dgp-enregistrerResourcesJeu-batch/src/main/java/com/dgp/enregistrerresourcesjeu/controller/EnregistrerResourcesJeuBatchController.java
@@ -4,7 +4,6 @@ import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.parameters.JobParameters;
 import org.springframework.batch.core.job.parameters.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobOperator;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
