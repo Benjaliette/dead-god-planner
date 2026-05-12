1aa64d638aaf7665ff5396a948a855d70b2ef54c
Benjamin Liet <benjamin.liet.dev@gmail.com>
2026-05-12 22:44:50 +0200

    FEAT : add Cors configuration to allow front to call api

---
+++ dgp-rest-api/src/main/java/com/dgp/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.dgp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(new String[]{"https://www.deadgodplanner.click", "https://deadgodplanner.click"})
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
