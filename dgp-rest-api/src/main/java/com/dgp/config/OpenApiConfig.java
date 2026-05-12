96440c6fb483a11f486561e934f4b21c2588eebb
Benjamin Liet <benjamin.liet.dev@gmail.com>
2026-05-12 18:47:34 +0200

    FEAT : add server url config for Swagger

--- dgp-rest-api/src/main/java/com/dgp/config/OpenApiConfig.java
+++ dgp-rest-api/src/main/java/com/dgp/config/OpenApiConfig.java
@@ -2,15 +2,22 @@ package com.dgp.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenApiConfig {
+    @Value("${app.openapi.server-url}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI()
+                .servers(List.of(new Server().url(serverUrl).description("API Server")))
                 .info(new Info()
                         .title("Dead God Planner API Documentation")
                         .version("0.1.0-SNAPSHOT")
